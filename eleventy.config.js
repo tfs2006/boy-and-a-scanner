72cbec0ce26d77e0a46949384dd1a6c81c09bebd
tfs2006 <fastwealth@4ourmedia.com>
2026-01-25 12:53:23 -0700

    Refactor: Migrate to Eleventy structure

---
+++ eleventy.config.js
@@ -0,0 +1,19 @@
+module.exports = function (eleventyConfig) {
+    // Pass through copy for static assets
+    eleventyConfig.addPassthroughCopy("src/**/*.svg");
+    eleventyConfig.addPassthroughCopy("src/**/*.png");
+    eleventyConfig.addPassthroughCopy("src/**/*.jpg");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
+    eleventyConfig.addPassthroughCopy("src/sitemap.xml");
+    eleventyConfig.addPassthroughCopy("src/CNAME");
+    eleventyConfig.addPassthroughCopy("src/google*.html");
+    eleventyConfig.addPassthroughCopy("src/css"); // CSS
+
+    return {
+        dir: {
+            input: "src",
+            output: "_site",
+            includes: "_includes"
+        }
+    };
+};
